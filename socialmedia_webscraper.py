f57850a0d7b4561ac557143b592b33b6fe3ef1d8
Vishal <vvishal@marshall.usc.edu>
2022-04-11 15:43:54 -0700

    Test for social media extraction working for test url

--- socialmedia_webscraper.py
+++ socialmedia_webscraper.py
@@ -11,26 +11,37 @@ df = pd.read_csv('URL_List.csv')
 
 print(df['List of URL'])
 
-'''
-
-#url = 'http://www.zello.com'
+url = 'http://www.zello.com'
 
 response = requests.get(url)
 
-target_sites = ['twitter.com', 'facebook.com']
+target_sites = ["facebook", "twitter"]
 sm_sites_present = []
 
 soup = BeautifulSoup(response.content, 'html.parser')
-all_links = soup.find_all('a', href = True)
+all_links = soup.find_all('meta')
+#print(all_links)
 
 
+sm_dict={}
 for sm_site in target_sites:
     for link in all_links:
-        if sm_site in link.attrs['href']:
-            sm_sites_present.append(link.attrs['href'])
+        #print(link.attrs.keys())
+        #print("Target Looking: ", sm_site)
+        if 'content' in link.attrs.keys(): # & sm_site in link.attrs['content']:
+            current_meta_content = link.attrs['content']
+            if sm_site in current_meta_content:
+                print('Found', current_meta_content)
+                split_list = current_meta_content.split('/') 
+                sm_dict[sm_site] = split_list[-1]            
+            #print(link.attrs['content'])
+            if 'name' in link.attrs.keys():
+                if sm_site in link.attrs['name'] and 'site' in link.attrs['name']:
+                    print('Found second', link.attrs['content'])
+                    current_meta_content = link.attrs['content'].replace('@','')
+                    sm_dict[sm_site]= current_meta_content
+            
+sm_sites_present.append(sm_dict)
 
-sm_sites_present = list(set(sm_sites_present))
+#sm_sites_present = list(set(sm_sites_present))
 print(sm_sites_present)
-
-
-'''
