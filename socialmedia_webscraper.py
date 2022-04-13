ad4de646e5c143f080e1d1c640eb29384e228096
Vishal <vvishal@marshall.usc.edu>
2022-04-12 21:06:09 -0700

    Scaling Capability using csv file implemented and added Exception Scenarios

--- socialmedia_webscraper.py
+++ socialmedia_webscraper.py
@@ -6,86 +6,96 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+from requests.exceptions import Timeout
+
 
 #Reading the list of URL from a CSV File -- makes is scalable to address 'n' number of URL
 df = pd.read_csv('URL_List.csv')
+#print(df['List of URL'])  #Uncomment this command to test the list of URL present as input
 
-print(df['List of URL'])
-
+'''
+#To use on individual URL Tests
 url = 'http://www.zello.com'
 url2 ="http://zynga.com"
 url3 ="https://www.appannie.com"
 url4 ="https://www.data.ai"
 url5 ="https://www.zello.com/downloads/android/"
+'''
+
+#Final List of Social Media and ID Dictionary in a list
+sm_sites_present = []
+
+target_sites = ["facebook", "twitter", "itunes" ,"play.google.com"]
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', \
             "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", \
                  "Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
-response = requests.get(url5, headers = headers, timeout=10)
-#print(response.text)
 
-#time.sleep(3)
+for url in df['List of URL']:
+    try:
+        response = requests.get(url, headers = headers, timeout=10)
+    except Timeout:
+        print("Timeout has been raised")
+        sm_sites_present.append('{}')
+        time.sleep(3)
+    except requests.exceptions.TooManyRedirects:
+        print("Too many redirects")
+        sm_sites_present.append('{}')
+        time.sleep(3)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+    
+    sm_dict={} #Empty Dictionary to create a json response for a URL
+    
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-#r = requests.get(response.headers['Location'])
-#print(r.status_code)
-
-
-
-target_sites = ["facebook", "twitter", "itunes" ,"play.google.com"]
-sm_sites_present = []
-
-soup = BeautifulSoup(response.content, 'html.parser')
-all_links = soup.find_all('meta')
-#print(all_links)
-
-
-sm_dict={}
-for sm_site in target_sites:
-    for link in all_links:
-        #print(link.attrs.keys())
-        #print("Target Looking: ", sm_site)
-        if 'content' in link.attrs.keys(): # & sm_site in link.attrs['content']:
-            current_meta_content = link.attrs['content']
-            if sm_site in current_meta_content:
-                print('Found', current_meta_content)
-                split_list = current_meta_content.split('/') 
-                sm_dict[sm_site] = split_list[-1]            
-            #print(link.attrs['content'])
-            if 'name' in link.attrs.keys():
-                if sm_site in link.attrs['name']:
-                    if 'site' in link.attrs['name']:
-                        print('Found second', link.attrs['content'])
-                        current_meta_content = link.attrs['content'].replace('@','')
-                        sm_dict[sm_site]= current_meta_content
+    #Cheking the Meta Tags First - High Probability of finding Facebook, Twitter and Apple ID as they are usually embedded in meta tags
+    all_links = soup.find_all('meta')   
+    for sm_site in target_sites:
+        for link in all_links:
+            #print(link.attrs.keys())
+            #print("Target Looking: ", sm_site)
+            if 'content' in link.attrs.keys(): 
+                current_meta_content = link.attrs['content']
+                if sm_site in current_meta_content:
+                    print('Found', current_meta_content)
+                    split_list = current_meta_content.split('/') 
+                    sm_dict[sm_site] = split_list[-1]            
+                #print(link.attrs['content'])
+                if 'name' in link.attrs.keys():
+                    if sm_site in link.attrs['name']:
+                        if 'site' in link.attrs['name']:
+                            print('Found second', link.attrs['content'])
+                            current_meta_content = link.attrs['content'].replace('@','')
+                            sm_dict[sm_site]= current_meta_content
                     
-                    #Checking for Itunes or Google Play Id in the meta
-                    elif 'app' in link.attrs['name']:
-                        print("Found App ID", link.attrs['content'])
+                        #Checking for Itunes or Google Play Id in the meta
+                        elif 'app' in link.attrs['name']:
+                            print("Found App ID", link.attrs['content'])
 
-                        #Extracting APP ID
-                        meta_contents = link.attrs['content'].split(',')
-                        for item in meta_contents:
-                            if "app-id" in item:
-                                app_id = item.split("=")[-1]
-                                if sm_site =="itunes":
-                                    sm_dict['ios'] = app_id
-                                else:
-                                    sm_dict['google'] = app_id
+                            #Extracting APP ID
+                            meta_contents = link.attrs['content'].split(',')
+                            for item in meta_contents:
+                                if "app-id" in item:
+                                    app_id = item.split("=")[-1]
+                                    if sm_site =="itunes":
+                                        sm_dict['ios'] = app_id
+                                    else:
+                                        sm_dict['google'] = app_id
 
-#Checking Separately for Google ID as they usually are in href
-all_tags = soup.find_all('a')
-for tag in all_tags:
-    if 'play.google.com' in tag.attrs['href']:
-        print("Found Google ID")
-        google_id = tag.attrs['href'].split('?')[-1]
-        sm_dict['google'] = google_id.split('=')[-1]
+    #Checking Separately for Google ID as they usually are in <a> tags inside href attribute that takes you to play.google.com
+    all_tags = soup.find_all('a')
+    for tag in all_tags:
+        if 'play.google.com' in tag.attrs['href']:
+            print("Found Google ID")
+            google_id = tag.attrs['href'].split('?')[-1]
+            sm_dict['google'] = google_id.split('=')[-1]
             
-sm_sites_present.append(sm_dict)
+    sm_sites_present.append(sm_dict)
 
-#sm_sites_present = list(set(sm_sites_present))
 print(sm_sites_present)
 
-print(type(sm_dict))
+#print(type(sm_dict))
 
 #json_format = json.dumps(sm_dict)
 #print(type(json_format))
